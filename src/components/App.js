0402e06bb7dd8c4dc280c5bb7b1236efa49b50a2
ahsanaaxioned <ahsana@axioned.com>
2023-03-16 16:04:45 +0530

    styling added in  react components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
+import "../assets/Style.css"
+import styles from  "../assets/Style.module.css"
+import styled from 'styled-components';
 function App() {
   const arr = ["samir","saif","saifudding","saif","ahsan"],
+  background = {
+    background: "#000"
+  },
   result = arr.map((item,idx)=>{
-    return <li key={idx}>{idx}{item}</li>
-  })
+    return <li key={idx}  className ={`${styles.test} ${styles.test2}`}>{idx}{item}</li>
+  }),
+  MyComponentsWrapper = styled.div`
+  max-width: 1366px;
+  width:80%;
+  margin: 0 auto
+  `
   return (
+    <>
+    <MyComponentsWrapper>
+    <h2 className="heading" style = {background}>hello and welcome in react practice</h2>
     <ul>{result}</ul>
+    </MyComponentsWrapper>
+    </>
+  
   );
 }
 
